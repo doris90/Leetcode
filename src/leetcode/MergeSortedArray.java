02d1a4486bd0cf895e2759ad12deaf927c7d8d44
Doris <243325989@qq.com>
2016-09-11 16:37:57 +0800

    update

--- src/leetcode/MergeSortedArray.java
+++ src/leetcode/MergeSortedArray.java
@@ -1,12 +1,17 @@
+/*
+ * 需求：将两个排序好的数组合并
+ * 思路：从后往前的归并排序；在目标数组中直接归并
+ * 		该思路同试用于两排序好链表合并*/
+
 package leetcode;
 
 public class MergeSortedArray {
 
 	public static void main(String[] args) {
-		int[] a = new int[1];
-		//a[0] = 1;
+		int[] a = new int[2];
+		a[0] = 2;
 		int[] b = {1};
-		merge(a, 0, b, 1);
+		merge(a, 1, b, 1);
 		//System.out.println();
 		/*
 		 *		for(int i=0; i<3; i++) {
@@ -16,44 +21,38 @@ public class MergeSortedArray {
 	}
 	
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-		int[] res = new int[nums1.length];
-		int p = 0;
-		int q = 0;
-		for(int i=0; i<res.length; i++) {
-			if(p != m && q != n) {
-				if(nums1[p] <= nums2[q]){
-					res[i] = nums1[p];
-					p++;
-					continue;
-				}
-				else {
-    				res[i] = nums2[q];
-    				q++;
+    	if(m == 0) {
+    		for(int i=0; i<n; i++) {
+    			nums1[i] = nums2[i];
+    		}
+    	}
+    	int p = m-1;
+    	int q = n-1;
+    	for(int i=(m+n-1); i>=0; i--) {
+    		if(p >= 0 && q >= 0) {
+    			if(nums1[p] >= nums2[q]) {
+    				nums1[i] = nums1[p];
+    				p--;
     				continue;
-				}
-			}
-			
-			if(p == m && q != n) {
-				res[i] = nums2[q];
-				q++;
+    			}
+    			else{
+    				nums1[i] = nums2[q];
+    				q--;
+    				continue;
+    			}
+    		}
+    		if(q >= 0) {
+				nums1[i] = nums2[q];
+				q--;
 				continue;
-			}
-			if(p != m && q == n) {
-				res[i] = nums1[p];
-				p++;
-				continue;
-			}
-		}
-		nums1 = res;
-		//for(int k=0; k<res.length; k++) {
-		  //  nums1[k] = res[k];
-		//}
-		sop(nums1);
+    		}
+    	}
+    	sop(nums1);
 	}
     
     public static void sop(int[] a) {
     	for(int i=0; i<a.length; i++) {
-    		System.out.print(a[i]);
+    		System.out.print(a[i] + " ");
     	}
     }
 
--- src/leetcode/MinimumDepthofBinaryTree.java
+++ src/leetcode/MinimumDepthofBinaryTree.java
@@ -15,7 +15,12 @@ public class MinimumDepthofBinaryTree {
 		// TODO Auto-generated method stub
 
 	}
-	
+	 public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	 }
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
         if(root.left == null && root.right != null) return minDepth(root.right) + 1;
